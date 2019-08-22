ee23fff4ab1a11a27b1a1176e97919433b32d3d1
Elie Zananiri <prisonerjohn@gmail.com>
2019-08-22 15:53:42 -0400

    Include ofConstants.h to pick up __WINDOWS_MM__ definition.

--- libs/RtMidi/src/src/RtMidi.cpp
+++ libs/RtMidi/src/src/RtMidi.cpp
@@ -36,6 +36,8 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 /**********************************************************************/
 
+#include "ofConstants.h"
+
 #include "RtMidi.h"
 #include <sstream>
 
